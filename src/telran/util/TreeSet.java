1b11750c064bf2b1304f8b06c69f00480a2b2a25
Yuragranovsky <yuragranovsky@gmail.com>
2021-11-15 10:16:15 +0200

    Initial commit

---
+++ src/telran/util/TreeSet.java
@@ -0,0 +1,103 @@
+package telran.util;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+public class TreeSet<T> extends AbstractSet<T> {
+ private static class Node<T> {
+	 T obj;
+	 Node<T> left; //reference to all nodes containing objects less than obj
+	 Node<T> right; //reference to all nodes containing objects greater than obj
+	 Node<T> parent; //reference to a parent
+	 Node(T obj) {
+		 this.obj = obj;
+	 }
+ }
+ private Node<T> root;
+ private Comparator<T> comp;
+ public TreeSet(Comparator<T> comp) {
+	 this.comp = comp;
+ }
+ @SuppressWarnings("unchecked")
+public TreeSet() {
+	 this((Comparator<T>)Comparator.naturalOrder());
+ }
+ private class TreeSetIterator implements Iterator<T> {
+//TODO required fields
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	 
+ }
+	@Override
+	public boolean add(T obj) {
+		if(root == null) {
+			addRoot(obj);
+			size++;
+			return true;
+		}
+			Node<T> parent = getParent(obj);
+			//If obj already exists getParent will return null
+			if (parent == null) {
+				return false;
+			}
+			Node<T> node = new Node<>(obj);
+			if (comp.compare(obj, parent.obj) < 0) {
+				parent.left = node;
+			} else {
+				parent.right = node;
+			}
+			node.parent = parent;
+			size++;
+		
+		return true;
+	}
+
+	private Node<T> getParent(T obj) {
+		Node<T> current = root;
+		Node<T> parent = null;
+		while(current != null) {
+			int res = comp.compare(obj, current.obj);
+			if (res == 0) {
+				return null;
+			}
+			parent = current;
+			current = res < 0 ? current.left : current.right;
+			
+		}
+		return parent;
+	}
+	private void addRoot(T obj) {
+		root = new Node<>(obj);
+		
+	}
+	@Override
+	public T remove(T pattern) {
+		// TODO next HW
+		return null;
+	}
+
+	
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(T pattern) {
+		
+		return getParent(pattern) == null ;
+	}
+
+}
