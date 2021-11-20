85885abe1b5e0425e1e59d49ff3b71584d73c640
ilyal <ilyallifsh@gmail.com>
2021-11-20 13:40:17 +0200

    commit this problem in junction test

--- src/telran/util/TreeSet.java
+++ src/telran/util/TreeSet.java
@@ -1,8 +1,9 @@
 package telran.util;
-//HW_12 IlyaL
+//HW_13 IlyaL
+
+
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.function.Predicate;
 
 public class TreeSet<T> extends AbstractSet<T> {
  private static class Node<T> {
@@ -23,47 +24,40 @@ public class TreeSet<T> extends AbstractSet<T> {
 public TreeSet() {
 	 this((Comparator<T>)Comparator.naturalOrder());
  }
- 
- private class TreeSetIterator<T> implements Iterator<T> {
- 
-	 Node<T> currentNode = getMostLeftNode((Node<T>) root);
- 
-	@Override
+ private Node<T> getMostLeftFrom(Node<T> from) {
+	 while(from.left != null) {
+		 from = from.left;
+	 }
+	 return from;
+ }
+ private Node<T> getFirstParentGreater(Node<T> node) {
+	 while(node.parent != null && node.parent.left != node) {
+		 node = node.parent;
+	 }
+	 return node.parent;
+ }
+ private class TreeSetIterator implements Iterator<T> {
+	 Node<T> current = root == null ? root : getMostLeftFrom(root);
+	 Node<T> previous = null;
+@Override
 	public boolean hasNext() {
- 
-		return currentNode != null;
-	 
+		
+		return current != null;
 	}
 
 	@Override
 	public T next() {
-		 
-		T res = currentNode.obj;
-		currentNode = currentNode.right != null ? 
-				getMostLeftNode(currentNode.right) :
-				getFirstGreaterParent(currentNode);	
+		T res = current.obj;
+		previous = current;
+		current = current.right != null ? getMostLeftFrom(current.right) :
+			getFirstParentGreater(current);
 		return res;
-	 
 	}
-	 
-	private Node<T> getMostLeftNode(Node<T> node) {
-		Node<T> mostLeftNode = node;
-		while (mostLeftNode != null && mostLeftNode.left != null) {
-			mostLeftNode = mostLeftNode.left;
-		}
-		return mostLeftNode;
-	}
-	
-	private Node<T> getFirstGreaterParent(Node<T> node) {
-		Node<T> parentNode = node;
-		int resComp = -100;
-		while(parentNode != null && resComp <= 0) {
-			resComp = ((Comparator<T>)comp).compare(parentNode.obj, node.obj);
-			if(resComp <= 0) {
-				parentNode = parentNode.parent;
-			}
-		}
-		return parentNode;		
+	@Override 
+	public void remove() {
+		//
+		TreeSet.this.remove(previous.obj);
+		
 	}
 	 
  }
@@ -111,16 +105,127 @@ public TreeSet() {
 	}
 	@Override
 	public T remove(T pattern) {
-		// TODO next HW
-		return null;
+		Node<T> removedNode = getNode(pattern);
+		if (removedNode == null) {
+			return null;
+		}
+ 		removeNodeSubst(removedNode);
+// 		removeNodeClassWork(removedNode);
+		return removedNode.obj;
 	}
 
-	
+	private void removeNodeSubst (Node <T> removedNode) {
+	if (removedNode.left!=null && removedNode.right!=null) {
+		//This is Junction!!!!
+		removeJunctionNode (removedNode);
+	} else {
+		//it's not junction or leaf
+		removeNoJunctionNode (removedNode);
+	}
+	}
+private void removeNoJunctionNode (Node<T> removedNode) {
+	// check root / not root
+	if (removedNode == root) {
+		removedNode.parent = null;
+		root = removedNode.right == null ? removedNode.left : removedNode.right;
+	} else {
+		//connect parent and child
+		Node<T> parent = removedNode.parent;
+		Node<T> child = removedNode.right == null ? removedNode.left : removedNode.right;
+		if (parent.right==removedNode) {
+			parent.right=child;
+		}else {	parent.left=child;}
+		if (child !=null) {child.parent = parent;}
 
+	}
+}
+	
+private void removeJunctionNode (Node<T> removedNode) {
+	// Search substNode is most left from right
+	Node<T> substitutionNode = getMostLeftFrom(removedNode.right); 
+	// Check root or not root
+	if (removedNode != root) {
+		Node<T> parent = removedNode.parent;
+		if (parent.right == removedNode) {
+			parent.right = substitutionNode;
+			} else {
+			parent.left = substitutionNode; 
+			}
+			substitutionNode.parent = parent;	
+	} else {
+		substitutionNode.parent = null;
+		root = substitutionNode;
+	}
+	
+	substitutionNode.left = removedNode.left;
+	substitutionNode.right = removedNode.right;
+	
+	if(substitutionNode.right != null) {
+			substitutionNode.right.parent = substitutionNode;	
+	}
+	if(substitutionNode.left != null) {
+		substitutionNode.left.parent = substitutionNode;	
+	}
+}
+	
+	
+	
+	private void removeNodeClassWork(Node<T> removedNode) {
+		//update the method by applying another algorithm
+		if (removedNode == root) {
+			removeRoot();
+		} else {
+			Node<T> parent = removedNode.parent;
+			Node<T> child = removedNode.right == null ? removedNode.left : removedNode.right;
+			
+			if (parent.right == removedNode) {
+				parent.right = child;
+				
+			} else {
+				parent.left = child;
+			}
+			if (child != null) {
+				child.parent = parent;
+			}
+			if (removedNode.right != null) {
+				Node<T> parentLeft = getMostLeftFrom(removedNode.right);
+				parentLeft.left = removedNode.left;
+				if(removedNode.left != null) {
+					removedNode.left.parent = parentLeft;
+				}
+				
+			}
+		}
+		size--;
+		
+	}
+	private void removeRoot() {
+		//update the method by applying another algorithm (see slide 28)
+		Node<T> child = root.right == null ? root.left : root.right;
+		if (child != null) {
+			child.parent = null;
+		}
+		if (root.right != null) {
+			Node<T> parentLeft = getMostLeftFrom(root.right);
+			parentLeft.left = root.left;
+			if (root.left != null) {
+				root.left.parent = parentLeft;
+			}
+		}
+		root = child;
+		
+	}
+	private Node<T> getNode(T pattern) {
+		Node<T> current = root;
+		while(current != null && !current.obj.equals(pattern)) {
+			current = comp.compare(pattern, current.obj) > 0 ? current.right : current.left;
+		}
+		return current;
+	}
 	@Override
 	public Iterator<T> iterator() {
 		
-		return ( new TreeSetIterator<T>());
+		return new TreeSetIterator();
 	}
 
 	@Override
